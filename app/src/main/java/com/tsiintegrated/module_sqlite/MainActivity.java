c2e2393e90e43926ee29176a39717796027258e1
Keeno <joseph.magtalas@gmail.com>
2017-10-30 22:25:44 +0800

    Initial commit

---
+++ app/src/main/java/com/tsiintegrated/module_sqlite/MainActivity.java
@@ -0,0 +1,129 @@
+package com.tsiintegrated.module_sqlite;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = " XOXO MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    EditText etName;
+    Button btnAdd;
+    TextView tvNoRecord;
+
+    ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabaseHelper = new DatabaseHelper(this);
+        etName = (EditText) findViewById(R.id.etName);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        tvNoRecord = (TextView) findViewById(R.id.tvNoRecord);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        populateListView();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = etName.getText().toString();
+                if (etName.length() != 0) {
+                    AddData(newEntry);
+                    etName.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+
+            }
+        });
+
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+            populateListView();
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void populateListView() {
+        Log.d(TAG, "populateListView: Displaying data in the ListView.");
+
+        //get the data and append to a list
+        Cursor data = mDatabaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
+
+        if(data.getCount() == 0){
+            mListView.setVisibility(View.GONE);
+            tvNoRecord.setVisibility(View.VISIBLE);
+        }else{
+            mListView.setVisibility(View.VISIBLE);
+            tvNoRecord.setVisibility(View.GONE);
+        }
+
+        while(data.moveToNext()){
+            //get the value from the database in column 1
+            //then add it to the ArrayList
+            listData.add(data.getString(1));
+        }
+        //create the list adapter and set the adapter
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+        mListView.setAdapter(adapter);
+
+        //set an onItemClickListener to the ListView
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = adapterView.getItemAtPosition(i).toString();
+                Log.d(TAG, "onItemClick: You Clicked on " + name);
+
+                Cursor data = mDatabaseHelper.getItemID(name); //get the id associated with that name
+                int itemID = -1;
+                while(data.moveToNext()){
+                    itemID = data.getInt(0);
+                }
+                if(itemID > -1){
+                    Log.d(TAG, "onItemClick: The ID is: " + itemID);
+                    Intent editScreenIntent = new Intent(MainActivity.this, EditDataActivity.class);
+                    editScreenIntent.putExtra("id",itemID);
+                    editScreenIntent.putExtra("name",name);
+                    startActivity(editScreenIntent);
+                }
+                else{
+                    toastMessage("No ID associated with that name");
+                }
+            }
+        });
+    }
+
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+}
